caa5463b7380fd59afa319d78e649f1f379f2144
Jessica Duarte <jessynd@Jessicas-MacBook-Air.local>
2014-03-08 19:30:36 -0500

    for Jana

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 var nn = {};
 
-nn.api-key = "7E776CF1C5DD322E58250545B073106D:5:68907953";
-nn.buri = "http://api.nytimes.com/svc/search/v2/articlesearch";
+nn.api_key = "7E776CF1C5DD322E58250545B073106D:5:68907953";
+nn.buri = "http://api.nytimes.com/svc/search/v2/articlesearch.json?";
+nn.userSearch = $("input.search").val()
+
+
+
+
+nn.getNews = function(){
+
+	$.ajax(nn.buri + "fq=" + nn.userSearch + "&api-key=" + nn.api_key , {
+		dataType: "jsonp",
+		type: "GET",
+		success : function(data){
+			console.log(data)
+			nn.displayNews(data.response)
+		} //end success
+	}); //end ajax
+
+} //end getNews
+
+
+
